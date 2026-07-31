7ee955a7e11a08a6380a16b15ab9bbba26414d26
apacheChatterjee <chatterjeemadhumita.pritha@gmail.com>
2026-07-31 09:38:14 +0530

    Use of Curly braces in Jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,28 @@ import './App.css'
 import User, {NamedComponent, UserKey} from './User'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let name = 'Madhumita'
+  const userObj = {
+      name:"Satya",
+      id:123,
+      email:"satya123@gmail.com",
+      phone:"2345437869"
+      }
+  const userArray = ['Harry','Potter','Julius','Cesar']
+  function display(a,b){
+      return a+b
+            }
 
   return (
     <>
-      <section id="center">
         <div>
-          <h1>Get started D DADA!</h1>
-          {/* This is <User/> component from user.jsx used as div or element here. Must starts with Capital Letter-> to tell React that its a component.*/}
-          <User/>
-          <NamedComponent/>
-          <h4>{UserKey}</h4>
+            <h1>Use of Curly braces in JSX</h1>
+            <h2>Name in the variable : {name}</h2>
+            <h2>Condition applied- {name ? name: 'user not found'}</h2>
+            <h2>Calling function and operation: {display(10,20)}</h2>
+            <h3>Fetching user object- {userObj.name} , {userObj.email}</h3>
+            <h3>Array- {userArray[2]}</h3>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
     </>
   )
 }
